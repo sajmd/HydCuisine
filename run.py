1e74fe9335f5ec6e69693759be7b65f2c7343d32
sajmd <sajidjava90@gmail.com>
2021-02-26 23:38:11 +0000

    Create the Flask App

--- run.py
+++ run.py
@@ -10,19 +10,19 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+@app.route("/shop")
+def shop():
+    return render_template("shop.html")
 
 
-@app.route("/careers")
-def careers():
-    return render_template("careers.html")
+@app.route("/donate")
+def donate():
+    return render_template("donate.html")
 
 
 if __name__ == "__main__":
